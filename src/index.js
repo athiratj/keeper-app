2a76b013cccccd25ffcb249fdb959d1599e7358a
Athira T J <athiratjthanaly83@gmail.com>
2020-08-06 21:16:04 +0530

    added functionalities adding and deleting notes

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-//5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
-//6. Make sure that the final website is styled like the example shown here:
-//https://w00gz.csb.app/
+//CHALLENGE:
+//1. Implement the add note functionality.
+//- Create a constant that keeps track of the title and content.
+//- Pass the new note back to the App.
+//- Add new note to an array.
+//- Take array and render seperate Note components for each item.
 
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
+//2. Implement the delete note functionality.
+//- Callback from the Note component to trigger a delete function.
+//- Use the filter function to filter out the item that needs deletion.
+//- Pass a id over to the Note component, pass it back to the App when deleting.
